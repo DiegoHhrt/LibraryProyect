85e084b035192ac3584b5f2282733b3ca8040563
Diego Hernandez <redbandito@outlook.com>
2021-05-08 12:28:45 -0500

    Feat: Maquetado formulario

---
+++ dynamics/accountCreation.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='UTF-8'>
+        <title>Creación de cuenta</title>
+    </head>
+    <body>
+        <form action='Index.php' method='POST'>
+            <fieldset>
+                <legend>Registro para la biblioteca</legend>
+                <label for="name"></label>
+                <label for="apellidoPat"></label>
+                <label for="apellidoMat"></label>
+                <label for="mail"></label>
+                <label for="type"></label>
+                <label for="birth"></label>
+                <label for="range"></label>
+                <label for="favorites"></label>
+            </fieldset>
+        </form>        
+    </body>
+</html>
