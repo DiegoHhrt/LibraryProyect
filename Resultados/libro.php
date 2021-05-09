8c92160e5e461972bc89f658395fb297d951365d
gerardo <gerardocervantesvillegas@gmail.com>
2021-05-09 16:06:02 -0500

    FEAT: maquetado para resultado de busqueda

---
+++ Resultados/libro.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Resultados de busqueda</title>
+    </head>
+</html>
+  
+</body>
+<h1>Datos del libro</h1>
+<table border="1">
+  <thead>
+  <th colspan="2"><strong>Datos</strong></th>
+  </thead>
+  <tbody>
+    <tr>
+      <td rowspan="3"><img src="https://imagessl4.casadellibro.com/a/l/t7/94/9788478887194.jpg" width="400" height="600" alt="Imagen de prueba"></td>
+      <td>Titulo: Aquí debe de ir el titulo xd</td>
+    </tr>
+    <tr>
+      <td>ID del libro: Aquí va todo xd</td>
+    </tr>
+    <tr>
+      <td>¿Datos extra?</td>
+    </tr>
+  </tbody>
+</table>
