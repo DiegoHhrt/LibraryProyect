24fff650edf73824e6eb6a5a592b1df9423996db
Diego Hernandez <redbandito@outlook.com>
2021-05-10 16:32:48 -0500

    Feat: Variables de sesión y validaciones de acceso

--- dynamics/Index.php
+++ dynamics/Index.php
@@ -1,42 +1,88 @@
 <?php
-    $sessionIn=(isset ($_POST["registred"]) && $_POST["registred"] !="") ?$_POST["registred"]: false;
+    //Se asigna a todas las variables de sesión un valor proviniente del formulario
+    function assignData ()
+    {
+        $name=(isset($_POST["name"]) && $_POST["name"] != "")? $_POST["name"]:"Inválido";
+        $apellP=(isset($_POST["apellidoPat"]) && $_POST["apellidoPat"] != "")? $_POST["apellidoPat"]:"Inválido";
+        $apellM=(isset($_POST["apellidoMat"]) && $_POST["apellidoMat"] != "")? $_POST["apellidoMat"]:"Inválido";
+        $id=(isset($_POST["identification"]) && $_POST["identification"] != "")? $_POST["identification"]:"Inválido";
+        $birth=(isset($_POST["birth"]) && $_POST["birth"] != "")? $_POST["birth"]:"Inválido";
+        $mail=(isset($_POST["mail"]) && $_POST["mail"] != "")? $_POST["mail"]:"Inválido";
+        $pw=(isset($_POST["psswd"]) && $_POST["psswd"] != "")? $_POST["psswd"]:"Inválido";
+        $fav=(isset($_POST["favorites"]) && $_POST["favorites"] != "")? $_POST["favorites"]:0;
+        $permit=(isset($_POST["permissions"]) && $_POST["permissions"] != "")? $_POST["permissions"]:'lec';
 
-if($sessionIn)
+        $_SESSION['init']=true;
+        $_SESSION['nombre']=$name;
+        $_SESSION['apellido']=$apellP." ".$apellM;
+        $_SESSION['identificacion']=$id;
+        $_SESSION['cumple']=$birth;
+        $_SESSION['email']=$mail;
+        $_SESSION['psswd']=$pw;
+        $_SESSION['favs']=$fav;
+        $_SESSION['tipoUsuario']=$permit;
+    }
+
+    session_name("register");
+    session_start();
+    
+    $sessionIn=(isset ($_POST["registred"]) && $_POST["registred"] !="") ?$_POST["registred"]: false;
+//En caso de haber cerrado sesión, redirige a la creación de una cuenta
+if(isset($_POST["closeS"]))
 {
-    echo "
-    <!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset='UTF-8'>
-                <title>Coyo Lectores</title>
-            </head>
-            <body>
-                <table>
-                    <tr>
-                        <td><img src='../statics/EscudoEnp6.jpg' alt='LogoP6' height='100'></td>
-                        <td><h1>Coyo Lectores ENP 6</h1></td>
-                    </tr>
-                </table>
-                <table align='right'>
-                    <tr>
-                        <td>
-                            <form action='./accountCreation.php' method='POST'>
-                                <label for='sessionInit'>
-                                    <button type='submit' value='sessionClose'>Cerrar sesión</button>
-                                </label>
-                            </form>
-                        </td>
-                    </tr>
-                </table>
-                <hr>
-                libros
-                <hr>
-                <p>Equipo de desarrollo: ''</p>
-                <p>Integrantes: </p>
-                <br>
-            </body>
-        </html>";
+    session_unset();
+    session_destroy();
+    header("location: ./accountCreation.php");
 }
+//Si existe una sesión activa o se ha creado una cuenta, despliega el contenido de la página
+elseif($sessionIn||(isset($_SESSION["init"])))
+{
+    //En caso de haberse recién creado una sesión, asigna los valores de las solicitudes http a variables de sesión y redirige a la misma imágen ya con los valores en variables de sesión
+    if($sessionIn)
+    {
+        assignData();
+        header("location: ./Index.php");
+    }
+    //Con variables de sesión existentes, depliega los contenidos de la página
+    if((isset($_SESSION["init"])))
+    {    
+    echo "
+        <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta charset='UTF-8'>
+                    <title>Coyo Lectores</title>
+                </head>
+                <body>
+                    <table>
+                        <tr>
+                            <td><img src='../statics/EscudoEnp6.jpg' alt='LogoP6' height='100'></td>
+                            <td><h1>Coyo Lectores ENP 6</h1></td>
+                        </tr>
+                    </table>
+                    <table align='right'>
+                        <tr>
+                            <td>
+                                <form action='./Index.php' method='POST'>
+                                    <label for='closeS'>
+                                        <button type='submit' name='closeS' value='sessionClose'>Cerrar sesión</button>
+                                    </label>
+                                </form>
+                            </td>
+                        </tr>
+                    </table>
+                    <hr>
+                    libros
+                    <hr>
+                    <p>Equipo de desarrollo: ''</p>
+                    <p>Integrantes: </p>
+                    <br>
+                </body>
+            </html>";
+            
+    }
+}
+//Si se trata de acceder a la vista principal sin una sesión activa o sin haber creado una sesión antes, te redirige al inicio de sesión
 else
 {
     header("location: ./accountCreation.php");
--- dynamics/accountCreation.php
+++ dynamics/accountCreation.php
@@ -1,107 +1,120 @@
 <?php
+//Antes de crear una cuenta, te permite seleccionar qué tipo de usuario es el que requieres.
+function permitsSelection()
+{
+    echo"
+        <form action='./accountCreation.php' method='POST'>
+            <fieldset>
+                <h2>Selecciona tu tipo de cuenta:</h2>
+                <br>
+                <label for='tipo'>
+                    <img src='../statics/defaultStudent.png' alt='Imagen estudiante' width='150'>
+                    <button type='submit' name='tipo' value='lector'>Lector</button>
+                    <img src='../statics/defaultLibrarian.png' alt='Imagen bibliotecario' width='150'>
+                    <button type='submit' name='tipo' value='bibliotec'>Bibliotecario</button>
+                    <img src='../statics/defaultAdmin.jpg' alt='Imagen admin' width='150'>
+                    <button type='submit' name='tipo' value='admin'>Administrador</button>
+                </label>
+            </fieldset>
+        </form>";
+}
 
-//ssion_name("register");
-//session_start();
+session_name("register");
+session_start();
 
-$accTipe=(isset ($_POST["tipo"]) && $_POST["tipo"] !="") ?$_POST["tipo"]: false;
-echo "
-<!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset='UTF-8'>
-            <title>Creación de cuenta</title>
-        </head>
-        <body>";
-        if($accTipe===false)
-        {
+if((isset($_SESSION["init"])))
+{
+    header("location: ./Index.php");
+}
+else{
+    $accTipe=(isset ($_POST["tipo"]) && $_POST["tipo"] !="") ?$_POST["tipo"]: false;
+    echo "
+    <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset='UTF-8'>
+                <title>Creación de cuenta</title>
+            </head>
+            <body>";
+            //Si no se ha seleccionado un tipo de usuario, debes elegir uno
+            if($accTipe===false)
+            {
+                permitsSelection();
+            }
+            //Una vez elegido el tipo de usuario, se permite rellenar el formulario de creación de cuenta
+            else
+            {
+                echo"
+                <form action='./Index.php' method='POST'>
+                    <fieldset>
+                        <legend>Registro para la biblioteca</legend>
+                        <p>Por favor, ingresa tu información de registro &#128214</p>
+                        <label for='name'>
+                            Nombre: <input type='text' name='name' placeholder='Nombre(s)' required>
+                        </label>
+                        <br><br>
+                        <label for='apellidoPat'>
+                            Apellido paterno: <input type='text' name='apellidoPat' placeholder='Apellido paterno' required>
+                        </label>
+                        <br><br>
+                        <label for='apellidoMat'>
+                            Apellido Materno: <input type='text' name='apellidoMat' placeholder='Apellido materno' required>
+                        </label>
+                        <br><br>
+                        <label for='identification'>";
+                            if($accTipe=="lector")
+                            {
+                                echo "Número de cuenta/RFC: <input type='text' name='identification' minlength='9' maxlength='14' required>";
+                            }
+                            elseif($accTipe=="bibliotec")
+                            {
+                                echo "RFC: <input type='text' name='identification' minlength='12' maxlength='14' required>";
+                            }
+                            elseif($accTipe=="admin")
+                            {
+                                echo "RFC: <input type='text' name='identification' minlength='12' maxlength='14' required>";
+                            }
+                        echo"
+                        </label>
+                        <br><br>
+                        <label for='mail'>
+                            Correo institucional: <input type='email' name='mail' placeholder='correo@comunidad.unam.mx' required>
+                        </label>
+                        <br><br>
+                        <label for='birth'>
+                            Fecha de nacimiento: <input type='date' name='birth' min='1920-01-01' max='".date('Y-m-d')."' required>
+                        </label>
+                        <br><br>
+                        <label for='psswd'>
+                            Nueva contraseña: <input type='password' name='psswd' required>
+                        </label>
+                        <label for='favorites'>
+                            <input type='hidden' name='favorites' value='0'>
+                        </label>
+                        <label for='permissions'>";
+                            if($accTipe=="lector")
+                            {
+                                echo "<input type='hidden' name='permissions' value='lec'>";
+                            }
+                            elseif($accTipe=="bibliotec")
+                            {
+                                echo "<input type='hidden' name='permissions' value='bibl'>";
+                            }
+                            elseif($accTipe=="admin")
+                            {
+                                echo "<input type='hidden' name='permissions' value='admon'>";
+                            }
+                        echo "</label>
+                        <br><br>
+                        <label for='registred'>
+                            <button type='submit' value='register' name='registred'>Registrarse</button>
+                        </label>
+                        <button type='reset'>Limpiar la información de los campos</button>
+                    </fieldset>
+                </form>";     
+            }   
             echo"
-            <form action='./accountCreation.php' method='POST'>
-                <fieldset>
-                    <h2>Selecciona tu tipo de cuenta:</h2>
-                    <br>
-                    <label for='tipo'>
-                        <img src='../statics/defaultStudent.png' alt='Imagen estudiante' width='150'>
-                        <button type='submit' name='tipo' value='lector'>Lector</button>
-                        <img src='../statics/defaultLibrarian.png' alt='Imagen bibliotecario' width='150'>
-                        <button type='submit' name='tipo' value='bibliotec'>Bibliotecario</button>
-                        <img src='../statics/defaultAdmin.jpg' alt='Imagen admin' width='150'>
-                        <button type='submit' name='tipo' value='admin'>Administrador</button>
-                    </label>
-                </fieldset>
-            </form>";
-        }
-        else
-        {
-            echo"
-            <form action='./Index.php' method='POST'>
-                <fieldset>
-                    <legend>Registro para la biblioteca</legend>
-                    <p>Por favor, ingresa tu información de registro &#128214</p>
-                    <label for='name'>
-                        Nombre: <input type='text' name='name' placeholder='Nombre(s)' required>
-                    </label>
-                    <br><br>
-                    <label for='apellidoPat'>
-                        Apellido paterno: <input type='text' name='apellidoPat' placeholder='Apellido paterno' required>
-                    </label>
-                    <br><br>
-                    <label for='apellidoMat'>
-                        Apellido Materno: <input type='text' name='apellidoMat' placeholder='Apellido materno' required>
-                    </label>
-                    <br><br>
-                    <label for='identification'>";
-                        if($accTipe=="lector")
-                        {
-                            echo "Número de cuenta/RFC: <input type='text' name='identification' minlength='9' maxlength='14' required>";
-                        }
-                        elseif($accTipe=="bibliotec")
-                        {
-                            echo "RFC: <input type='text' name='identification' minlength='12' maxlength='14' required>";
-                        }
-                        elseif($accTipe=="admin")
-                        {
-                            echo "RFC: <input type='text' name='identification' minlength='12' maxlength='14' required>";
-                        }
-                    echo"
-                    </label>
-                    <br><br>
-                    <label for='mail'>
-                        Correo institucional: <input type='email' name='mail' placeholder='correo@comunidad.unam.mx' required>
-                    </label>
-                    <br><br>
-                    <label for='birth'>
-                        Fecha de nacimiento: <input type='date' name='birth' min='1920-01-01' max='".date('Y-m-d')."' required>
-                    </label>
-                    <br><br>
-                    <label for='psswd'>
-                        Nueva contraseña: <input type='password' name='psswd' required>
-                    </label>
-                    <label for='favorites'>
-                        <input type='hidden' name='favorites' value='0'>
-                    </label>
-                    <label for='permissions'>";
-                        if($accTipe=="lector")
-                        {
-                            echo "<input type='hidden' name='permissions' value='lec'>";
-                        }
-                        elseif($accTipe=="bibliotec")
-                        {
-                            echo "<input type='hidden' name='permissions' value='bibl'>";
-                        }
-                        elseif($accTipe=="admin")
-                        {
-                            echo "<input type='hidden' name='permissions' value='admon'>";
-                        }
-                    echo "</label>
-                    <br><br>
-                    <label for='registred'>
-                        <button type='submit' value='register' name='registred'>Registrarse</button>
-                    </label>
-                    <button type='reset'>Limpiar la información de los campos</button>
-                </fieldset>
-            </form>";     
-        }   
-        echo"
-        </body>
-    </html>";
+            </body>
+        </html>";
+}
 ?>
