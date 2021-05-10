e6f34b2351da01e0b29f9a086a62b7c4e8b2429e
Diego Hernandez <redbandito@outlook.com>
2021-05-10 15:44:01 -0500

    Feat: Maquetado base vistas de admin

--- dynamics/addOrDelete.php
+++ dynamics/addOrDelete.php
@@ -1,10 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Vista de usuarios</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Vista de usuarios</title>
+    </head>
+    <body>
+        <h2>Gestión de usuarios de la biblioteca</h2>
+        <hr>
+        <?php
+            $x=0;
+            while($x<10)
+            {
+                $y=$x+1;
+                echo "<a href=''>Usuario".$y."</a><br><br>";
+                $x++;
+            }
+        ?>    
+        <hr>
+    </body>
 </html>
--- dynamics/reports.php
+++ dynamics/reports.php
@@ -1,10 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Reportes de contenido</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Reportes de contenido</title>
+    </head>
+    <body>
+        <h2>Historial de reportes de contenido inadecuado.</h2>
+        <hr>
+        <?php
+            $x=0;
+            while($x<10)
+            {
+                $y=$x+1;
+                echo "<a href=''>Reporte".$y."</a><br><br>";
+                $x++;
+            }            
+        ?>   
+        <hr>
+    </body>
 </html>
